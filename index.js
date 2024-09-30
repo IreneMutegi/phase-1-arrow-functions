2d1428b39a695c874c106df4e8a2051f32209244
Irene Mutegi <irene@Ms-Echo.>
2024-09-30 13:25:04 +0300

    arrow functions

---
+++ index.js
@@ -0,0 +1,10 @@
+const divide = function(a,b) {
+    return a/b
+}
+console.log(divide(2000,100))
+
+const square = x => x*x;
+console.log(square(4));
+
+const add= (x,y)=> x+y 
+console.log(add(2,3))
